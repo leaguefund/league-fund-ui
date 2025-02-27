0ca639dd1f9b4d3abcbfdfa68a81dcb6dc935463
CryptJS13 <cryptjs13@protonmail.com>
2025-02-27 16:10:04 -0700

    Populate header data

--- src/components/AuxiliaryHeader.tsx
+++ src/components/AuxiliaryHeader.tsx
@@ -1,37 +1,32 @@
 "use client";
 
 import React from 'react';
-// import { useGlobalState } from '@/context/GlobalStateContext';
-// import { getLeagueName, getLeagueDues } from '@/utils/onChainReadUtils';
+import { useGlobalState } from '@/context/GlobalStateContext';
+import { getLeagueName, getLeagueTotalBalance } from '@/utils/onChainReadUtils';
 import { useSidebar } from '@/context/SidebarContext';
 
 const AuxiliaryHeader: React.FC = () => {
-  // const { state } = useGlobalState();
+  const { state } = useGlobalState();
   const { isExpanded } = useSidebar();
   const [leagueName, setLeagueName] = React.useState<string>('');
-  const [leagueDues, setLeagueDues] = React.useState<number>(0);
-  console.log(setLeagueName, setLeagueDues)
+  const [leagueBalance, setLeagueBalance] = React.useState<number>(0);
 
   React.useEffect(() => {
     const fetchLeagueInfo = async () => {
-      // TODO: This is where we will fetch the league info from the contract
-
-      // const contractAddress = state.leagueSelected?.contractAddress;
-      // if (contractAddress) {
-      //   try {
-      //     const name = await getLeagueName(contractAddress);
-      //     const dues = await getLeagueDues(contractAddress);
-      //     setLeagueName(name);
-      //     setLeagueDues(dues);
-      //   } catch (error) {
-      //     console.error('Error fetching league info:', error);
-      //   }
-      // }
+      const contractAddress = state.selectedLeagueAddress;
+      if (contractAddress) {
+        try {
+          const name = await getLeagueName(contractAddress);
+          const balance = await getLeagueTotalBalance(contractAddress);
+          setLeagueName(name);
+          setLeagueBalance(balance);
+        } catch (error) {
+          console.error('Error fetching league info:', error);
+        }
+      }
     };
-
     fetchLeagueInfo();
-  }, []);
-  // }, [state.leagueSelected?.contractAddress]);
+  }, [state.selectedLeagueAddress]);
 
   return (
     <header className="sticky top-[57px] flex w-full bg-white border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
@@ -43,7 +38,7 @@ const AuxiliaryHeader: React.FC = () => {
         </h2>
         <div className="bg-gray-50 rounded-lg dark:bg-gray-800 px-2 py-1">
           <span className="text-base font-medium text-gray-800 dark:text-white">
-            ${leagueDues}
+            ${leagueBalance}
           </span>
         </div>
       </div>
