43da54336b508891a9c9c210ba1de98d1fa07bf8
Shannon Moranetz <shannonrmoranetz@gmail.com>
2025-02-27 12:10:34 -0700

    updates to header

--- src/components/AuxiliaryHeader.tsx
+++ src/components/AuxiliaryHeader.tsx
@@ -1,33 +1,37 @@
 "use client";
 
 import React from 'react';
-import { useGlobalState } from '@/context/GlobalStateContext';
-import { getLeagueName, getLeagueDues } from '@/utils/onChainReadUtils';
+// import { useGlobalState } from '@/context/GlobalStateContext';
+// import { getLeagueName, getLeagueDues } from '@/utils/onChainReadUtils';
 import { useSidebar } from '@/context/SidebarContext';
 
 const AuxiliaryHeader: React.FC = () => {
-  const { state } = useGlobalState();
+  // const { state } = useGlobalState();
   const { isExpanded } = useSidebar();
   const [leagueName, setLeagueName] = React.useState<string>('');
   const [leagueDues, setLeagueDues] = React.useState<number>(0);
+  console.log(setLeagueName, setLeagueDues)
 
   React.useEffect(() => {
     const fetchLeagueInfo = async () => {
-      const contractAddress = state.leagueSelected?.contractAddress;
-      if (contractAddress) {
-        try {
-          const name = await getLeagueName(contractAddress);
-          const dues = await getLeagueDues(contractAddress);
-          setLeagueName(name);
-          setLeagueDues(dues);
-        } catch (error) {
-          console.error('Error fetching league info:', error);
-        }
-      }
+      // TODO: This is where we will fetch the league info from the contract
+
+      // const contractAddress = state.leagueSelected?.contractAddress;
+      // if (contractAddress) {
+      //   try {
+      //     const name = await getLeagueName(contractAddress);
+      //     const dues = await getLeagueDues(contractAddress);
+      //     setLeagueName(name);
+      //     setLeagueDues(dues);
+      //   } catch (error) {
+      //     console.error('Error fetching league info:', error);
+      //   }
+      // }
     };
 
     fetchLeagueInfo();
-  }, [state.leagueSelected?.contractAddress]);
+  }, []);
+  // }, [state.leagueSelected?.contractAddress]);
 
   return (
     <header className="sticky top-[57px] flex w-full bg-white border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
