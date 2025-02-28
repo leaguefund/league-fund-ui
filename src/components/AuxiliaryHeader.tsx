07459c02bfd5c290c7a79b8cdb7e2ef004d20b7f
Shannon Moranetz <shannonrmoranetz@gmail.com>
2025-02-27 19:10:49 -0700

    fix background color

--- src/components/AuxiliaryHeader.tsx
+++ src/components/AuxiliaryHeader.tsx
@@ -29,7 +29,7 @@ const AuxiliaryHeader: React.FC = () => {
   }, [state.selectedLeagueAddress]);
 
   return (
-    <header className="sticky top-[57px] flex w-full bg-white border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
+    <header className="sticky top-[57px] flex w-full !bg-gray-800/40 border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
       <div className={`flex items-center justify-between w-full px-5 py-3 lg:px-6`}>
         <h2 className="text-base font-medium text-gray-800 dark:text-white truncate">
           {leagueName || 'Loading...'}
