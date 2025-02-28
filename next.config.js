ae6d882fcc580fc9014b3c1ef6fce4dfe6b324d7
Shannon Moranetz <shannonrmoranetz@gmail.com>
2025-02-28 13:40:52 -0700

    update domains

--- next.config.js
+++ next.config.js
@@ -1,31 +1,56 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'www.figma.com',
-      'sleepercdn.com',
-      'avatars.githubusercontent.com',
-      'mir-s3-cdn-cf.behance.net',
-      'preview.redd.it',
-      'img.freepik.com',
-      'cdn4.vectorstock.com',
-      'oaidalleapiprodscus.blob.core.windows.net',
-      's3.amazonaws.com',
-      's3-us-west-1.amazonaws.com',
-      's3-us-west-2.amazonaws.com',
-      's3-us-east-1.amazonaws.com',
-      's3-us-east-2.amazonaws.com',
-      's3.us-west-1.amazonaws.com',
-      's3.us-west-2.amazonaws.com',
-      's3.us-east-1.amazonaws.com',
-      's3.us-east-2.amazonaws.com',
-      'd1.awsstatic.com',
-      'd2.awsstatic.com',
-      'd3.awsstatic.com',
-      'cloudfront.net',
-      'd1cdn.aws.com',
-      'd2cdn.aws.com',
-      'd3cdn.aws.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+        pathname: '/private/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.awsstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.aws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.figma.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sleepercdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mir-s3-cdn-cf.behance.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'preview.redd.it',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn4.vectorstock.com',
+      }
     ],
   },
   webpack(config) {
