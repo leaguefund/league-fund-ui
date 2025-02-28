cbb138f1e55fca808733451a2a36979408557d0a
Shannon Moranetz <shannonrmoranetz@gmail.com>
2025-02-28 13:36:43 -0700

    update domain whitelist

--- next.config.js
+++ next.config.js
@@ -1,7 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.figma.com', 'sleepercdn.com', 'avatars.githubusercontent.com', 'mir-s3-cdn-cf.behance.net', 'preview.redd.it', 'img.freepik.com', 'cdn4.vectorstock.com'],
+    domains: [
+      'www.figma.com',
+      'sleepercdn.com',
+      'avatars.githubusercontent.com',
+      'mir-s3-cdn-cf.behance.net',
+      'preview.redd.it',
+      'img.freepik.com',
+      'cdn4.vectorstock.com',
+      'oaidalleapiprodscus.blob.core.windows.net',
+      's3.amazonaws.com',
+      's3-us-west-1.amazonaws.com',
+      's3-us-west-2.amazonaws.com',
+      's3-us-east-1.amazonaws.com',
+      's3-us-east-2.amazonaws.com',
+      's3.us-west-1.amazonaws.com',
+      's3.us-west-2.amazonaws.com',
+      's3.us-east-1.amazonaws.com',
+      's3.us-east-2.amazonaws.com',
+      'd1.awsstatic.com',
+      'd2.awsstatic.com',
+      'd3.awsstatic.com',
+      'cloudfront.net',
+      'd1cdn.aws.com',
+      'd2cdn.aws.com',
+      'd3cdn.aws.com'
+    ],
   },
   webpack(config) {
     // SVG Configuration
